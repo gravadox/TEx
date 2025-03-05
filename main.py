a87c40127f348e6ff98eb70c1ef38a3af4206762
Gravadox <primordialobject@gmail.com>
2025-03-05 19:09:48 +0300

    resizable window ✅

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from app import TEx
 if __name__ == "__main__":
     root = ctk.CTk()
     root.iconbitmap('icon.ico')
-    root.resizable(False, False)
+    root.resizable(True, True)
+    
+    root.minsize(width=1000, height=400) 
+    
     app = TEx(master=root)
     root.protocol("WM_DELETE_WINDOW", app.minimize_to_tray)
     root.geometry("1100x600")
